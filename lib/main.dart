6cdbad3572f96be1936c4e6f5d8554327766d996
Chandrakant <chandrakantsandu@partner.samsung.com>
2023-06-30 10:37:29 +0530

    test changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-
   runApp(
     ProviderScope(
       child: EasyLocalization(
